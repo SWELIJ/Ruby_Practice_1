647eda818ab9a09dc240bfe300604d0c167ad07a
SWELIJ <elijahtsheridan25@gmail.com>
2024-09-30 13:13:53 -0400

    added hello world strings

---
+++ playground.rb
@@ -0,0 +1,10 @@
+# address = [1,2,3,4,5,6,7,8,9]
+# p address # p for print
+
+# p address[5] #prints index 5
+# new_address = address.reverse!
+# p new_address
+
+puts "Hello World" # -- output a string (s) Hello World
+p "Hello world" #outputs "Hello World" (with quotations)
+print "Hello World" #prints Hello World (negating quotations)
